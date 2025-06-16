5e88b5ad9f1de06498204826d99ce99fc17ad760
张舰 <zhangjian.chandan@bytedance.com>
2025-06-16 09:56:45 +0800

    chore: update webpack config

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,11 +1,14 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
       hot: true,
-      static: './'
+      static: path.join(__dirname, '../'),
+       // dev-server port
+      port: process.env.PORT || 4000,
   }
 });
